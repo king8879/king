e9b2283bc140f49d78419971b68ba215dec26993
king8879 <k4507244@gmail.com>
2025-05-30 19:41:46 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,52 @@
+import os
+from os import getenv
+
+
+
+# ------------------------------------------------
+API_ID = int(os.environ.get("API_ID", "18579024"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH","124981da628d86e21ee492da77cd4037")
+# ------------------------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN","7787054704:AAETBgejOge_76rU8vkxO7kjCem8woQ2GPE")
+# ------------------------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+BOT_TEXT = " 🫵"
+# ------------------------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "7726207129"))
+# ------------------------------------------------
+# //LOG CHANNEL ID 
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002517535092"))
+
+# //FORCE_CHANNEL_ID
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002517535092")) 
+# ------------------------------------------------
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://vikassonawale0:JWyQFas7vlG1bkaL@cluster0.beermge.mongodb.net/?retryWrites=true&w=majority")
+# -----------------------------------------------
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002517535092"))
+# -----------------------------------------------
+join = ''
+# -----------------------------------------------
+UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
+# -----------------------------------------------
+UNSPLASH_QUERY = 'animal baby'
+# -----------------------------------------------
+ADMIN_BOT_USERNAME = "" #without @
+
+THUMB_URL = os.environ.get("THUMB_URL", "https://envs.sh/9HT.jpg")
+
+
+
+
+# # Bot configuration
+# API_ID = int(os.environ.get("API_ID", "18579024"))
+# API_HASH = os.environ.get("API_HASH", "124981da628d86e21ee492da77cd4037")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7787054704:AAETBgejOge_76rU8vkxO7kjCem8woQ2GPE")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "7726207129"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7726207129").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002517535092"))
+# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://vikassonawale0:JWyQFas7vlG1bkaL@cluster0.beermge.mongodb.net/?retryWrites=true&w=majority")
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002517535092"))
+# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
+
